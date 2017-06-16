f68f22c99b57ff11ac88a6b6fce462cebdf5d529
Alexander Meise <rkan001@gmail.com>
2017-06-16 10:13:21 +0200

    Update modify_redirects.ps1

--- modify_redirects.ps1
+++ modify_redirects.ps1
@@ -1,11 +1,42 @@
-Import-CSV  D:\Data\redirectlist.csv -Header alias,destination | Foreach-Object{
+#Creates backup folder if not present
+if (!(Test-Path D:\Data\backups)){
+mkdir D:\Data\backups
+ Write-Host "Backup folder created"
+ Write-Host ""
+ }
+   
+Import-CSV  D:\Data\alias.csv -Header alias,destination | Foreach-Object{
+    
+    #prepare alias destination and folder
     $nalias = $_.alias
     $ndestination = $_.destination
-
-    Write-Host "For the alias: "$nalias 
-    Write-Host "The new destination is: "$ndestination
-  .\appcmd.exe set config $nalias -section:system.webServer/httpRedirect /destination:$ndestination
+    $folder = "redirect_"+$nalias
+    
+    
+    #if web.config exists
+    if ((Test-Path D:\Webroot\${folder}\web.config)){
+    
+    #write the process    
+    Write-Host "--------------------------------------"
+    Write-Host "Processing alias: "$nalias
+   
+    #make a backup 
+    Copy-Item D:\Webroot\${folder}\web.config D:\DATA\backups\${folder}.backup
+    Write-Host "A BACKUP of the web.config was performed, please find it in D:\Data\backups"
+    
+    #checks the old destination and print it
+    #$old = .\appcmd.exe list config $folder -section:system.webServer/HttpRedirect 
+    #$parsed = [regex]::matches($old,'(?<=\").+?(?=\")').value
+    #Write-Host "The current destination is: " $parsed[2]
+   
+    #prints the new destination and modifies it
+    Write-Host "The new destination will be: "$ndestination
+    .\appcmd.exe set config $folder -section:system.webServer/httpRedirect /destination:$ndestination
+    Write-Host ""
+    }else{
+     Write-Host "------------------------------------------------------"
+     Write-Host $nalias "does not exist or could not be found on this server" 
+     Write-Host "------------------------------------------------------"     
+     }
+    
 }
-
-#needs to be executed as an admin on C:\WINDOWS\System32\inetsrv (better with PS ISE)
-#D:\Data\redirectlist.csv: alias1,destination1 (1 per line)
