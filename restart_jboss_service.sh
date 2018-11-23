1947fef0e99ae0aebc92c8905cf1629a80b01b65
Alexander Meise <rkan001@gmail.com>
2018-11-23 10:57:46 +0100

    Update and rename restart_service.sh to restart_jboss_service.sh

--- restart_jboss_service.sh
+++ restart_jboss_service.sh
@@ -24,14 +24,14 @@ echo -e "\n Service restart started" >> $LOG_NAME
 echo "Proceeding with service stop"
 service $SERVICE stop
 echo "sleep 60 seconds..."
-sleep 20
+sleep 60
 echo "renaming ${JBOSS_LOG_FILE} to ${JBOSS_LOG_FILE}.${SHORT_DATE}"
 mv $JBOSS_LOG_FILE $JBOSS_LOG_FILE.$SHORT_DATE
 echo "Renamed, sleep 10 seconds ..."
-sleep 10
+sleep 20
 echo "Proceeding with service start"
 service $SERVICE start
-sleep 20
+sleep 40
 echo -e "\n Service restart finished" >> $LOG_NAME
 
 }
@@ -39,9 +39,20 @@ echo -e "\n Service restart finished" >> $LOG_NAME
 
 email_notification(){
 
-        echo -e "Date: $(date) \nServer: $(uname -n)     \nService: $SERVICE    \nStatus: $(service $SERVICE status) \n\nDF -H:\n\n $(df -h)" | mailx -s "JBOSS RESTART" foo@bar.com,foo2@bar.com
-        echo -e "\n EMAIL SENT TO ADMIN" >> $LOG_NAME
-                echo -e "\n EMAIL SENT TO ADMIN"
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 2 ))
+then
+echo -e "Date: $(date) \nServer: $(uname -n)     \nService: $SERVICE    \nStatus: RUNNING FINE! \n\nDF -H:\n\n $(df -h)" | mailx -s "JBOSS RESTART" mail@alexandermeise.com
+echo -e "\n EMAIL SENT TO ADMIN, SERVICE IS RUNNING" >> $LOG_NAME
+echo -e "\n EMAIL SENT TO ADMIN, SERVICE IS RUNNING"
+
+else
+
+echo -e "Date: $(date) \nServer: $(uname -n)     \nService: $SERVICE    \nStatus: NOT RUNNING!! \n\nDF -H:\n\n $(df -h)" | mailx -s "JBOSS RESTART" mail@alexandermeise.com
+echo -e "\n EMAIL SENT TO ADMIN, SERVICE IS NOT RUNNING" >> $LOG_NAME
+echo -e "\n EMAIL SENT TO ADMIN, SERVICE IS NOT RUNNING"
+
+fi
+
 }
 
 delete_old_logs(){
